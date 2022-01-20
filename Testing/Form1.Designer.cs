a10c11be92488c9c7be1e4ba600b8414bbef84c9
George Winser <georgewinser@gmail.com>
2022-01-20 22:09:43 +0000

    Fixed Typo
    Fixed 'Rigth' to 'Right'

--- Testing/Form1.Designer.cs
+++ Testing/Form1.Designer.cs
@@ -475,7 +475,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(756, 265);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Rigth Trigger";
+            this.tabPage2.Text = "Right Trigger";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // r_n_7
